7840fa5ee5a80417cc8261909926b843c8c37cfd
Jonathan White <mrwhite@Jonathans-MacBook-Pro-2.local>
2015-10-11 00:31:33 -0400

    added sec

---
+++ config/secrets.js
@@ -0,0 +1,9 @@
+var config = {
+	accountSid: 'ACc51b59e56e3c7a9c5a666e6458b55f0d',
+	authToken: '24a78fa77ede420db8901b584036909f',
+	port: '3000',
+	phoneNumber: '+16787838456',
+	jonathanPhoneNumber: '+17703297606'
+};
+
+module.exports = config;
